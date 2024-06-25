14197a594adb499dedcfb90fa6d57095b3a68df9
Ibezimmichael <ibezim.michael@yahoo.com>
2024-06-25 14:11:04 +0100

    Update docker compose file add redis config

--- django_k8s/django_k8s/settings.py
+++ django_k8s/django_k8s/settings.py
@@ -95,10 +95,9 @@ DB_IS_AVAIL = all([
 ])
 
 
-POSTGRES_READY = str(os.environ.get("POSTGRES_READY")) == "1"
 
 
-if DB_IS_AVAIL and POSTGRES_READY:
+if DB_IS_AVAIL:
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.postgresql",
